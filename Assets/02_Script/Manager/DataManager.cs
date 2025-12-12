c596ac2c1d3dde6b6881a5c31ec2339ab4fda1c4
JeongHyun <wlsrurdmlwjdrnf@gmail.com>
2025-12-12 15:22:33 +0900

    CSVReader 추가

--- Assets/02_Script/Manager/DataManager.cs
+++ Assets/02_Script/Manager/DataManager.cs
@@ -22,14 +22,11 @@ public enum ItemType
 
 public class DataManager : Singleton<DataManager>
 {
-    private const string DATA_PATH = "06_Data"; // 모든 csv파일 데이터들이 모여있는 폴더명
+    private const string DATA_PATH = "Data"; // 모든 csv파일 데이터들이 모여있는 폴더명
 
 
-    //private const string ITEM_DATA_PATH = "ItemData"; // 아이템 데이터 csv파일명
-    //private const string CHAPTER_DATA_PATH = "ChapterData"; // 챕터 데이터 csv파일명
-
-    //private List<ItemData> itemData = new List<ItemData>(); // 아이템 데이터를 저장할 컨테이너
-    //private List<ChapterData> chapterData = new List<ChapterData>(); // 챕터 데이터를 저장할 컨테이너
+    private const string ITEM_DATA_PATH = "ItemData"; // 아이템 데이터 csv파일명
+    private List<ItemData_> itemData = new List<ItemData_>(); // 아이템 데이터를 저장할 컨테이너
 
 
 
@@ -40,12 +37,7 @@ public class DataManager : Singleton<DataManager>
 
     private void LoadItemData()
     {
-        
-    }
-
-    private void LoadChapterData()
-    {
-
+       
     }
 }
 
